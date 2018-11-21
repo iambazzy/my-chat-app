dfddd67249173d9dca01c78d6dbe9f2239b53ddc
iambazzy <basittammy@gmail.com>
2018-11-21 13:10:10 +0530

    'update'

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
-const express = require('express');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-var app = express();
-const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
 
-app.listen(PORT,()=>{
+app.get('/index',(req,res)=>{
+  res.sendFile(publicPath + '/index.html');
+});
+
+io.on('connection',(socket)=>{
+  console.log('New User Connected');
+
+  socket.on('disconnect',()=>{
+    console.log('User Was Disconnected');
+  });
+});
+
+http.listen(PORT,()=>{
   console.log(`Server Is Running On Port ${PORT}`);
 })
