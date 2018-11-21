fc02c40325ad88bd674f0ad9986c3dc5b6973f0f
iambazzy <basittammy@gmail.com>
2018-11-21 14:06:10 +0530

    'customEvents'

--- server/server.js
+++ server/server.js
@@ -6,14 +6,26 @@ const io = require('socket.io')(http);
 const publicPath = path.join(__dirname, '../public');
 const PORT = process.env.PORT || 3000;
 
-
+// route index.html
 app.get('/index',(req,res)=>{
   res.sendFile(publicPath + '/index.html');
 });
 
+// connection established
 io.on('connection',(socket)=>{
   console.log('New User Connected');
 
+// sending message from server
+  socket.emit('newMessage',{
+    text: 'Hey this is my first Message'
+  });
+
+// recieving message from the client
+  socket.on('createMessage',(data)=>{
+    console.log('got a message',data);
+  });
+
+// on dis-connection
   socket.on('disconnect',()=>{
     console.log('User Was Disconnected');
   });
